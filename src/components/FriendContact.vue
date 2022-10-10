1d29762f40300b0485a6a64ee239fd457ca20079
Miles Inada <94945665+milesinada@users.noreply.github.com>
2022-10-09 15:05:33 -1000

    Vue Components

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,31 @@
+<template>
+    <li>
+        <h2>{{friend.name}}</h2>
+        <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>        
+        <ul v-if="detailsAreVisible">
+            <li><strong>Phone:</strong>{{friend.phone}}</li>
+            <li><strong>EMail:</strong>{{friend.email}}</li>
+        </ul>
+    </li>
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            detailsAreVisible: false,
+            friend: {
+                id: 'miles',
+                name: 'Miles Inada',
+                phone: '8083469737',
+                email: 'milesinada219@gmail.com'
+            },
+        };
+    },
+    methods: {
+        toggleDetails(){
+            this.detailsAreVisible = !this.detailsAreVisible;
+        },
+    }
+};
+</script>
